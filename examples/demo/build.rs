912d70a8b37e096f38c345df9dcd7a3ed26b54ee
Leon Schuermann <leon@is.currently.online>
2024-11-14 11:45:47 -0500

    WIP

--- examples/demo/build.rs
+++ examples/demo/build.rs
@@ -6,14 +6,13 @@ fn main() {
     println!("cargo:rerun-if-changed=./c_src/demo.c");
     println!("cargo:rerun-if-changed=./c_src/demo.h");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
+    let cflags = std::env::var("EF_BINDGEN_CFLAGS")
+	.expect("Please set EF_BINDGEN_CFLAGS");
+
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
         .header("c_src/demo.h")
-        .clang_arg("-isystem ../../libtock-newlib-4.3.0.20230120/riscv/riscv64-unknown-elf/include")
+	// TODO: this is brittle and will break on args that have spaces in them!
+        .clang_args(cflags.split(" "))
         .rustfmt_configuration_file(Some(
             PathBuf::from("./rustfmt-bindgen.toml")
                 .canonicalize()
@@ -23,18 +22,10 @@ fn main() {
             PathBuf::from("./demo.encapfn.toml").canonicalize().unwrap(),
         ))
         .use_core()
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    //
-    // We avoid using OUT_DIR as this does not allow us to view the
-    // intermediate artifacts.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("libdemo_bindings.rs"))
