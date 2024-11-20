2cdafb830010e88e7a71885a323d6a4367ac3acd
Leon Schuermann <leon@is.currently.online>
2024-11-19 19:59:57 -0500

    shell.nix: pin default Nixpkgs revision

--- shell.nix
+++ shell.nix
@@ -2,7 +2,16 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 # Copyright Tock Contributors 2024.
 
-{ pkgs ? import <nixpkgs> {}}:
+let
+  # Nixpkgs revision including a missing `rust-darwin-setup.bash` script in the
+  # rustup derivation:
+  pinnedNixpkgsSrc = builtins.fetchTarball {
+    url = "https://github.com/lschuermann/nixpkgs/archive/95a65bf0a34045b57787676c0c10765604a7ccd7.tar.gz";
+    sha256 = "sha256:0j3pyxx6ib4vnpj875j5a5wij2vr9ks8rnywhvhd7m14y9qmpz0f";
+  };
+
+in
+{ pkgs ? import pinnedNixpkgsSrc {} }:
 
 with builtins;
 let
